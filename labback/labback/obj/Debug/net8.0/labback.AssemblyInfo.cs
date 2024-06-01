34bece7787e95b34f14325429300af690ab89985
delfinaplakolli <delfinaplakolliii@gmail.com>
2024-06-01 22:45:54 +0500

    Token

--- labback/labback/obj/Debug/net8.0/labback.AssemblyInfo.cs
+++ labback/labback/obj/Debug/net8.0/labback.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("labback")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538b1a9ce3664b9b84e7df73b756189aee81d845")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c26b181942005f4aba6cb1a3ae0f5b210394721")]
 [assembly: System.Reflection.AssemblyProductAttribute("labback")]
 [assembly: System.Reflection.AssemblyTitleAttribute("labback")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
